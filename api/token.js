b7531a648612633c7681c79dd3d2da0b8382cc2e
sdeevanapalli <s.deevanapalli@gmail.com>
2025-08-16 00:47:14 +0530

    update

---
+++ api/token.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      grant_type: "refresh_token",
+      refresh_token: process.env.SPOTIFY_REFRESH_TOKEN,
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      client_secret: process.env.SPOTIFY_CLIENT_SECRET
+    })
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
