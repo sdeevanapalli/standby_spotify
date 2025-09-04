21d9aa5da287a86aaa3186a98ebce47b506a3dc5
sdeevanapalli <s.deevanapalli@gmail.com>
2025-09-04 19:26:26 +0530

    refactor: non crit errors

--- public/script.js
+++ public/script.js
@@ -5,12 +5,13 @@ async function getCurrentlyPlaying() {
   const data = await res.json();
 
   if (data && data.item) {
-    document.getElementById('album-art').src = data.item.album.images[0].url;
+    const albumArtEl = document.getElementById('album-art');
+    albumArtEl.src = data.item.album.images[0].url;
     document.getElementById('track-name').textContent = data.item.name;
     document.getElementById('artist-name').textContent = data.item.artists.map(a => a.name).join(', ');
 
-    albumArt.onload = () => {
-      const [r, g, b] = colorThief.getColor(albumArt); // dominant color
+    albumArtEl.onload = () => {
+      const [r, g, b] = colorThief.getColor(albumArtEl); // dominant color
       document.body.style.background = `linear-gradient(to bottom, rgba(${r},${g},${b},0.7), #000)`;
     };
   }
